067e8417f5572baf7015957ccdd5b18460f92294
mahmoud saad <mahmoud.saaad137@gmail.com>
2026-04-30 02:46:48 +0300

    Clean up: remove old Next.js files, keep static HTML/CSS/JS only

--- src/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const STORE_NAME = process.env.STORE_NAME || "عطارة اليرموك";
-export const WHATSAPP_NUMBER = process.env.WHATSAPP_NUMBER || "201158221296";
-export const SHIPPING_COST = Number(process.env.SHIPPING_COST || "50");
-export const CURRENCY = process.env.CURRENCY || "ج.م";
